7f9afc0bd7c73262072209718da2acd301909965
Ashan Perera <ashan.perera1@pearson.com>
2023-10-23 15:41:18 +0530

    more data added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const nextConfig = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        env_variable_1: "value 1",
+        env_variable_2: "value 2",
+        env_variable_3: "value 3",
+      },
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+  };
+};
+
+module.exports = (phase) => nextConfig(phase);
---
+++ posts/getting-started-2.md
@@ -0,0 +1,13 @@
+---
+title: "getting started 2 nextjs"
+date: "2023-10-26"
+author: "ashan"
+image: "getting-started-nextjs.png"
+excerpt: "jdkslajdlksjladkjslad"
+slug: "getting-started-2"
+isFeatured: false
+---
+
+# this is a title
+
+this is sme [link](https://www.udemy.com/)
